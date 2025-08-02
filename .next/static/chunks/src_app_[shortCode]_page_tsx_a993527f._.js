8ccc047ff7c8d8d1dfdd01004ce01d611b70e9c2
EuZicado <129035588+EuZicado@users.noreply.github.com>
2025-08-02 00:20:52 -0300

    Add .next/static/chunks/src_app_[shortCode]_page_tsx_a993527f._.js 1754104848529

---
+++ .next/static/chunks/src_app_[shortCode]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_8a672f40._.js",
+  "static/chunks/src_296e08f7._.js"
+],
+    source: "dynamic"
+});
